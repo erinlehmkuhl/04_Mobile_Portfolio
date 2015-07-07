b4363c27f33382d03c55f5f0c5eda8317ce9e0ef
Erin Lehmkuhl <erinlehmkuhl@gmail.com>
2015-07-06 19:34:30 -0700

    refactor: remove dead code in main.js. -removed caching from html. it's too annoying. -cleaned up main.js - had some non-functioning code -css for oversized screens to float container and center pizzas (some js)

--- views/js/main.js
+++ views/js/main.js
@@ -1,22 +1,22 @@
-//creates a confined area for pizza to inhabit
-var canvasWidth = document.querySelector("#movingPizzas1").offsetWidth;
+//limits amount of pizzas drawn
+var canvasWidth = document.querySelector("#header").offsetWidth;
+var canvasLeft = canvasWidth - (canvasWidth/2);
 
 if (canvasWidth < 350) {
-	cols = 2;
+	var cols = 2;
 }else if (canvasWidth > 450 && canvasWidth < 800) {
-	cols = 3;
+	var cols = 3;
 }else{
-	cols = 8;
+	var cols = 8;
 }
 
 //starts scroll information
 var lastKnownScrollY = 4;
-var canvasLeftEdge = document.querySelector(".container").offsetLeft;
 var ticking = false;
 
 
 // runs updatePositions on scroll
-window.addEventListener('scroll', onScroll);
+window.addEventListener("scroll", onScroll);
 
 //captures scroll information
 function onScroll() {
@@ -33,32 +33,28 @@ function requestTick() {
 }
 
 // Moves the sliding background pizzas based on scroll position
-function updatePositions(cols) {
+function updatePositions() {
 	var currentScrollY = lastKnownScrollY;
 	var items = document.getElementsByClassName("mover");
 	for (var i = 0; i < items.length; i++) {
 		var phase = Math.sin((currentScrollY / 30000) + (i % cols));
-		items[i].style.transform = "translateX(" + 1400 * (phase) + "px)";
+		items[i].style.transform = "translateX(" + 500 * (phase) + "px)";
 	}
 	ticking = false;
 }
 
-//ensures pizzas start being drawn near the left side of the .container
-window.addEventListener("resize", function(){
-	canvasLeftEdge = document.querySelector(".container").offsetLeft;
-});
-
-// Generates the sliding pizzas when the page loads.
+//Generates the sliding pizzas when the page loads.
 var s = 256;
-for (var i = 0; i < 30; i++) {
-	var elem = document.createElement('img');
-	elem.basicLeft = ((i % cols) * s )+ canvasLeftEdge;
-	elem.className = 'mover';
+var multip = 300;
+for (var i = 0; i < 80; i++) {
+	var randNum = Math.floor((Math.random() * multip) + canvasLeft);
+	var elem = document.createElement("img");
+	elem.className = "mover";
 	elem.src = "images/conformed/pizza.png";
 	elem.style.height = "100px";
 	elem.style.width = "73.333px";
-	elem.style.top = (Math.floor(i / cols) * s) + 'px';
+	elem.style.top = (Math.floor(i / cols) * s) + "px";
+	elem.style.left = randNum + "px";
 	document.querySelector("#movingPizzas1").appendChild(elem);
-	updatePositions(cols);
+	updatePositions();
 }
-
